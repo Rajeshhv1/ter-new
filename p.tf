0828af536ad6ecad002b2ef7bf5c71b162569830
Rajeshhv1 <hvrajesh1@gmail.com>
2023-02-09 17:59:27 +0530

    added tf files

---
+++ p.tf
@@ -0,0 +1,9 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id   = "89b6ca4e-5b64-4c7f-a39e-e1bbea2d850f"
+  tenant_id         = "bb3cb068-28d1-4f00-9bb2-31933d49cb80"
+  client_id         = "da9cda2c-593f-4822-82a2-53eee0d61cf1"
+  client_secret     = "AVe8Q~k2JswFcBOXVsDqeiD7vedbP1YZQMALUajW"
+}
+
